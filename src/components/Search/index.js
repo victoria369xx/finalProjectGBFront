1363deffdb5ae6ca5be6f479305ea1e9a1dccf39
TataDr <druzhkova.tn@gmail.com>
2022-08-17 11:07:25 +0300

    Revert "search with useInput"
    This reverts commit 2f2c679c1702c123408a76b8d66bbebff7a6b27c.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,53 +1,68 @@
-import { Box, Button, Grid, MenuItem, TextField } from "@mui/material";
-import { Link } from "react-router-dom";
-import { getCityArrFromDB } from "../../helpers/getData";
-import { getSearchResult } from "../../store/search/actions";
-import { useInput } from "../../utils/useInput";
+
+import * as React from 'react';
+import { Box, Button, Grid, MenuItem, TextField } from "@mui/material"
+import { Link } from "react-router-dom"
+import { getCityArrFromDB } from "../../helpers/getData"
+import { useState } from "react"
+import { getSearchResult } from "../../store/search/actions"
+
 
 export const Search = () => {
-  const cities = getCityArrFromDB();
 
-  const { value: cityId, handleChange: changeId, reset: resetId } = useInput(1);
+    const cities = getCityArrFromDB()
+    console.log(cities)
 
-  const handleSubmit = (event) => {
-    getSearchResult(cityId);
-  };
+    const [cityName, setCityName] = useState('Москва')
 
-  return (
-    <Grid item xs={12} md={12}>
-      <Box
-        component="form"
-        sx={{
-          display: "flex",
-          "& > :not(style)": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
-      >
-        <TextField
-          id="filled-select-currency"
-          select
-          label="Выберите город"
-          //   value={cityId}
-          onChange={changeId}
-          variant="filled"
-        >
-          {/* ___________ Исправить, когда появится Api ____________*/}
+    const handleChange = (event) => {
+        const elTarget = event.target.value
+        setCityName(elTarget)
+    }
+    let currencyCity = {}
+    cities.filter(el => {
+        if (el.city == cityName) { currencyCity = el }
 
-          {cities.map((city) => (
-            <MenuItem key={city.id} value={city.id}>
-              {city.city}
-            </MenuItem>
-          ))}
-        </TextField>
+    })
 
-        <Button id="search-btn" type="submit" variant="outlined">
-          <Link to={`search/${cityId}`} style={{ textDecoration: "none" }}>
-            Поиск{" "}
-          </Link>
-        </Button>
-      </Box>
-    </Grid>
-  );
-};
+    const path = `/search/${currencyCity.id}`
+
+    return (
+
+        <Grid item xs={12} md={12}>
+            <Box
+                component="form"
+                sx={{
+                    display: "flex",
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+
+                <TextField
+                    id="filled-select-currency"
+                    select
+                    label="Выберите город"
+                    value={cityName}
+                    onChange={handleChange}
+                    variant="filled"
+
+                >
+                    {/* ___________ Исправить, когда появится Api ____________*/}
+
+                    {cities.map((city) => (
+                        <MenuItem key={city.id} value={city.city}>
+                            {city.city}
+                        </MenuItem>
+                    ))}
+                </TextField>
+
+
+                <Button id="search-btn" type="submit" variant="outlined" onClick={getSearchResult(currencyCity.id)}>
+                    <Link to={path} style={{ textDecoration: 'none' }}>Поиск </Link></Button>
+
+            </Box>
+        </Grid >
+
+    )
+}
