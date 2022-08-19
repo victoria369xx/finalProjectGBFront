ac8085526d4b41de371c3f817c42875a04b1e2e6
TataDr <druzhkova.tn@gmail.com>
2022-08-19 12:29:11 +0300

    test connect with backend

--- src/routes/ProjectRoutes/index.js
+++ src/routes/ProjectRoutes/index.js
@@ -7,24 +7,14 @@ import { NotFound } from "../../pages/NotFound";
 import { PrivateRoute } from "../PrivateRoute";
 
 export const ProjectRoutes = () => {
-<<<<<<< HEAD
-  const dispatch = useDispatch();
-  const authState = useSelector(getIsAuth);
-
-  useEffect(() => {
-    dispatch(initAuthAction);
-  });
-=======
   const authStatus = false;
->>>>>>> test connect with backend
   return (
     <Routes>
       <Route path="/" element={<HomePage />}>
         <Route path="/login" element={<LogInPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route element={<PrivateRoute authed={authStatus} />}>
-          {/* <Route exact path={"profile"} element={<ProfilePage />} /> */}
+          <Route exact path={"profile"} element={<ProfilePage />} />
         </Route>
         <Route path={"*"} element={<NotFound />} />
       </Route>
