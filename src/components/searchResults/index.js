b52b5d97155ed274fb06582efd37b4e71b1b9d59
TataDr <druzhkova.tn@gmail.com>
2022-08-20 18:20:48 +0300

    проверка рендера поиска

--- src/components/searchResults/index.js
+++ src/components/searchResults/index.js
@@ -1,33 +1,34 @@
-import "./renderSearch_module.css"
+import { useSelector } from "react-redux";
+import { selectSearchResult } from "../../store/search/selector";
+import "./renderSearch_module.css";
 
-export const RenderSearchResultsBlock = (data) => {
-    console.log(`data: ${[data]}`);
+export const RenderSearchResultsBlock = () => {
+  const result = useSelector(selectSearchResult);
+  console.log(result);
 
-    return (
-
-        <section class="page-wrapper">
-            <h2 class="text-lev2 text-center">Наши догситтеры</h2>
-            <div class="grid-card">
-                <a href="#" class="card">
-                    <img src="https://picsum.photos/200/300" alt="" />
-                    <div class="card-content">
-                        <h3 class="text-lev3">Наталья</h3>
-                        <p>У Лукоморья дуб зеленый. Златая цепь на дубе том. И днем. и ночью кот ученый все ходит по цепи кругом</p>
-                        <span>
-                            <div class="address text-additional">г. Кременчуг-Константиновское</div>
-                            <div class="address text-additional">ул. Центральная, д. 37</div>
-                        </span>
-                    </div>
-                    <div class="card-info">
-
-                        <div class="price">
-                            от 1500 ₽
-                    </div>
-                        <button class="btn">89999999999</button>
-                    </div>
-                </a>
-            </div>
-        </section>
-    )
-}
-
+  return (
+    <section class="page-wrapper">
+      <h2 class="text-lev2 text-center">Наши догситтеры</h2>
+      <div class="grid-card">
+        <a href="#" class="card">
+          <img src="https://picsum.photos/200/300" alt="" />
+          <div class="card-content">
+            <h3 class="text-lev3">{result.name}</h3>
+            <p>
+              У Лукоморья дуб зеленый. Златая цепь на дубе том. И днем. и ночью
+              кот ученый все ходит по цепи кругом
+            </p>
+            <span>
+              <div class="address text-additional">{result.city}</div>
+              <div class="address text-additional">{result.adress}</div>
+            </span>
+          </div>
+          <div class="card-info">
+            <div class="price">от 1500 ₽</div>
+            <button class="btn">{result.phone}</button>
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
