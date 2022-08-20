544896d70160f042e58713f225622f90d0872632
TataDr <druzhkova.tn@gmail.com>
2022-08-20 17:45:45 +0300

    исправление ошибок

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,13 +1,12 @@
-
-import React, {useState} from 'react';
+// import React, { useState } from "react";
 import { Box, Button, Grid, MenuItem, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { getCities, getSearchResult } from "../../store/search/actions";
 import { selectCities } from "../../store/search/selector";
-import { Link } from "react-router-dom"
-import { getCityArrFromDB } from "../../helpers/getData"
+import { Link } from "react-router-dom";
+import { getCityArrFromDB } from "../../helpers/getData";
 
 export const Search = () => {
   const navigate = useNavigate();
@@ -72,64 +71,61 @@ export const Search = () => {
   );
 };
 
+// export const Search = () => {
 
-export const Search = () => {
+//     const cities = getCityArrFromDB()
+//     console.log(cities)
 
-    const cities = getCityArrFromDB()
-    console.log(cities)
+//     const [cityName, setCityName] = useState('Москва')
 
-    const [cityName, setCityName] = useState('Москва')
+//     const handleChange = (event) => {
+//         const elTarget = event.target.value
+//         setCityName(elTarget)
+//     }
+//     let currencyCity = {}
+//     cities.filter(el => {
+//         if (el.city == cityName) { currencyCity = el }
 
-    const handleChange = (event) => {
-        const elTarget = event.target.value
-        setCityName(elTarget)
-    }
-    let currencyCity = {}
-    cities.filter(el => {
-        if (el.city == cityName) { currencyCity = el }
+//     })
 
-    })
+//     const path = `/search/${currencyCity.id}`
 
-    const path = `/search/${currencyCity.id}`
+//     return (
 
-    return (
+//         <Grid item xs={12} md={12}>
+//             <Box
+//                 component="form"
+//                 sx={{
+//                     display: "flex",
+//                     '& > :not(style)': { m: 1, width: '25ch' },
+//                 }}
+//                 noValidate
+//                 autoComplete="off"
+//             >
 
-        <Grid item xs={12} md={12}>
-            <Box
-                component="form"
-                sx={{
-                    display: "flex",
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
+//                 <TextField
+//                     id="filled-select-currency"
+//                     select
+//                     label="Выберите город"
+//                     value={cityName}
+//                     onChange={handleChange}
+//                     variant="filled"
 
-                <TextField
-                    id="filled-select-currency"
-                    select
-                    label="Выберите город"
-                    value={cityName}
-                    onChange={handleChange}
-                    variant="filled"
+//                 >
+//                     {/* ___________ Исправить, когда появится Api ____________*/}
 
-                >
-                    {/* ___________ Исправить, когда появится Api ____________*/}
+//                     {cities.map((city) => (
+//                         <MenuItem key={city.id} value={city.city}>
+//                             {city.city}
+//                         </MenuItem>
+//                     ))}
+//                 </TextField>
 
-                    {cities.map((city) => (
-                        <MenuItem key={city.id} value={city.city}>
-                            {city.city}
-                        </MenuItem>
-                    ))}
-                </TextField>
+//                 <Button id="search-btn" type="submit" variant="outlined" onClick={getSearchResult(currencyCity.id)}>
+//                     <Link to={path} style={{ textDecoration: 'none' }}>Поиск </Link></Button>
 
+//             </Box>
+//         </Grid >
 
-                <Button id="search-btn" type="submit" variant="outlined" onClick={getSearchResult(currencyCity.id)}>
-                    <Link to={path} style={{ textDecoration: 'none' }}>Поиск </Link></Button>
-
-            </Box>
-        </Grid >
-
-    )
-}
-
+//     )
+// }
--- src/routes/ProjectRoutes/index.js
+++ src/routes/ProjectRoutes/index.js
@@ -6,25 +6,23 @@ import { LogInPage } from "../../pages/LogInPage";
 import { SignUpPage } from "../../pages/SignUpPage";
 import { HomePage } from "../../pages/HomePage";
 import { AccountPage } from "../../pages/AccountPage";
-import { RenderSearchResultsBlock } from '../../components';
-
-
+import { RenderSearchResultsBlock } from "../../components";
+import { ProfilePage } from "../../pages/ProfilePage";
+import { NotFound } from "../../pages/NotFound";
 
 export const ProjectRoutes = () => {
-    const authStatus = false
-    return (
-        <Routes>
-            <Route path='/' element={<HomePage />}>
-                <Route path='/login' element={<LogInPage />} />
-                <Route path='/signup' element={<SignUpPage />} />
-                <Route element={<PrivatRoute authed={authStatus} />}>
-                    <Route exact path={"profile"} element={<ProfilePage />} />
-                </Route>
-                <Route path='/search/:cityId' element={<RenderSearchResultsBlock />} />
-                <Route path={"*"} element={<NotFound />} />
-
-            </Route>
-        </Routes>
-
-    )
-}
+  const authStatus = false;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route element={<PrivatRoute authed={authStatus} />}>
+          <Route exact path={"profile"} element={<ProfilePage />} />
+        </Route>
+        <Route path="/search/:cityId" element={<RenderSearchResultsBlock />} />
+        <Route path={"*"} element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
