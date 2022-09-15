bf841702ab6cbc8051802b257a829f19d94637da
TataDr <druzhkova.tn@gmail.com>
2022-09-16 00:13:23 +0300

    add change password function

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -136,7 +136,6 @@ export const Search = () => {
                   onChange={handlerChangeSize}
                   MenuProps={{
                     sx: {
-                      padding: "0",
                       "&& .MuiPaper-root": {
                         boxShadow:
                           "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
