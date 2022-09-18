01af9c3207658e2ab8a116a65ef65a24ad48aa21
Victoria <v.shurkalina@gmail.com>
2022-09-18 12:24:36 +0400

    desktop version fixed

--- src/components/DownloadApp/index.js
+++ src/components/DownloadApp/index.js
@@ -4,12 +4,12 @@ import androidApp from '../../appSrc/_PetBooking_16013570.apk';
 
 export const DownloadApp = () => {
     return (
-        <div className='container'>
+        <div className='container page-wrapper'>
             <div className='download_wrapperFlex'>
                 <div className='download_leftBlock'>
-                    <h2 className='download_heading leftBlock_el'>Скачайте приложение для Android</h2>
+                    <h2 className='download_heading'>Скачайте приложение для Android</h2>
                     <img className='leftBlock_el' src={android} alt="иконка андроид"/>
-                    <a className='leftBlock_el' href={androidApp} download>Скачать</a>
+                    <a className='leftBlock_el leftBlock_link' href={androidApp} download>Скачать</a>
                 </div>
                 <div className='download_rightBlock'>
                 <h2 className='download_heading'> Инструкция по скачиванию</h2>
